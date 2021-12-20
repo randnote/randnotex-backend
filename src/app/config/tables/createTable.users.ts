38f48adb58b5a1331cd749b045f61f7bfdedb114
danielromeo <danielromeo99@gmail.com>
2021-12-20 22:08:07 +0200

    fixed the firstname database type

--- src/app/config/tables/createTable.users.ts
+++ src/app/config/tables/createTable.users.ts
@@ -1,7 +1,7 @@
 // admins table:
 export let createUsers: string = `create table if not exists users(
     id int primary key auto_increment,
-    firstname int not null,
+    firstname varchar(200) not null,
     lastname varchar(200)not null,
     email varchar(200)not null,
     password varchar(255) not null,
