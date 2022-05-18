12499e1a6ecda99574bd95ba4581b46b389c225b
danielromeo <danielromeo99@gmail.com>
2022-05-18 14:21:38 +0200

    changed the backend db tables

--- src/app/config/tables/createTable.transactions.ts
+++ src/app/config/tables/createTable.transactions.ts
@@ -1,16 +1,17 @@
 export let createTransactionBlockchain: string = `create table if not exists transactionsBlockhain(
     id int primary key auto_increment,
     user_id int not null,
-    publicAddress MEDIUMTEXT not null,
-    amount decimal(10,8) not null
+    fromAddress MEDIUMTEXT not null,
+    toAddress MEDIUMTEXT not null,
+    notes decimal(10,8) not null
 )`;
 
 export let createTransactionWebsite: string = `create table if not exists transactionsWebsite(
     id int primary key auto_increment,
     user_id int not null,
     price decimal(10,2) not null,
-    type enum ('buy','sell') not null,  
-    amount decimal(10,2) not null
+    ordertype enum ('buy','sell') not null,  
+    amount decimal(10,2) not null,
+    notes decimal(10,8) not null
 )`
-// price represents the price of the coin at the time...
-// type is whether they are buying or selling...
+
