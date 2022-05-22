d61b9562352cabb513466ecb6bd3232198f8f23c
danielromeo <danielromeo99@gmail.com>
2022-05-22 22:17:24 +0200

    caught the bad supply error.. now can user any initial supply number

--- src/app/price.ts
+++ src/app/price.ts
@@ -1,11 +1,12 @@
 import Axios from "axios";
+import { Console } from "console";
 
 let PRICE: number = 1000;
-let CURRENT_SUPPLY : number = 100;
+let CURRENT_SUPPLY : number = 100; // careful buddy, this starting supply can cause infite numbers if not set correctly
 
 // this function just calls the blockchain to get the number of notes in circulation
 const getSupply = async() =>{
-    let supply = 0;
+    let supply: number = 0;
 
     // remember to add env variable here...
 	await  Axios.get(`http://localhost:8033/supply`).then(
@@ -21,16 +22,30 @@ const getSupply = async() =>{
 
 const calculatePrice = async() =>{
     let NEW_SUPPLY: number = await getSupply();
-
+    if(NEW_SUPPLY === 0 ){
+        console.log("caught ya")
+        NEW_SUPPLY = 100;
+    }
     // price based off of increased supply:
     // check if old mySupply is still the same as the new, if not, it means that it increased and so we decrease the price:
+   
+   console.log(CURRENT_SUPPLY)
+   console.log(NEW_SUPPLY)
     if(CURRENT_SUPPLY !==  NEW_SUPPLY){
         
         let s: number = CURRENT_SUPPLY/NEW_SUPPLY;
         s= s * 100;
         PRICE = PRICE * s/100;
         CURRENT_SUPPLY = NEW_SUPPLY;
+        console.log({
+            s: s,
+            price: PRICE,
+            CURRENT_SUPPLY: CURRENT_SUPPLY,
+            NEW_SUPPLY: NEW_SUPPLY
+        })
     }
+
+    // now , write an influencer when user buys and sells....
 
     return PRICE;
 }
